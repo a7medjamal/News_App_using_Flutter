4a3c7c482455bbaf525774d5318c235202f060e4
a7medjamal <ahmedjmal20003@gmail.com>
2024-12-11 20:20:30 +0200

    Optimized Image loading from internet

--- lib/widgets/news_tile.dart
+++ lib/widgets/news_tile.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:easy_url_launcher/easy_url_launcher.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/models/article_model.dart';
@@ -24,11 +25,13 @@ class NewsTile extends StatelessWidget {
           onTap: () => _launchURL(article.url),
           child: ClipRRect(
             borderRadius: BorderRadius.circular(12),
-            child: Image.network(
-              article.Image,
+            child: CachedNetworkImage(
+              imageUrl: article.Image,
               height: 200,
               width: double.infinity,
               fit: BoxFit.cover,
+              placeholder: (context, url) => Center(child: CircularIndicator()),
+              errorWidget: (context, url, error) => Icon(Icons.error),
             ),
           ),
         ),
